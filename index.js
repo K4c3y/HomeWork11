f2448618f59f77cd6b34c8b946f92b5f1aaf7d97
Kacey Churchill <kaceychurchill@Kaceys-Air-3.lan>
2020-05-05 16:38:50 -0400

    setup index.html and node server running

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+const PORT = process.env.PORT || 5000; 
+const output_dir = path.resolve(__dirname, 'public');
 
-// required modules 
-const express = require('express'); 
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// setting static folder 
 
-//calling express
-const app  = express();
-
-
-app.set('view engine', 'ejs');
-
-// root urk (/)
-app.get('/', function(req, res){
-    res.render('index');
- });
-
-   
-
-
-// server is listening on port 3000 
-
-app.listen(3000, function () {
-    console.log('example app listing on port 3000')
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(output_dir, 'index.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.listen(PORT, () => console.log('Sever Started'))
+
